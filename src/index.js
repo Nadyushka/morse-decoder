5a516f77e82a883bafe9c8425f28ae8c628747c1
Nadya Baranova <12nadya11@gmail.com>
2022-07-19 20:30:04 +0300

    add: add solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,100 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+	'.-': 'a',
+	'-...': 'b',
+	'-.-.': 'c',
+	'-..': 'd',
+	'.': 'e',
+	'..-.': 'f',
+	'--.': 'g',
+	'....': 'h',
+	'..': 'i',
+	'.---': 'j',
+	'-.-': 'k',
+	'.-..': 'l',
+	'--': 'm',
+	'-.': 'n',
+	'---': 'o',
+	'.--.': 'p',
+	'--.-': 'q',
+	'.-.': 'r',
+	'...': 's',
+	'-': 't',
+	'..-': 'u',
+	'...-': 'v',
+	'.--': 'w',
+	'-..-': 'x',
+	'-.--': 'y',
+	'--..': 'z',
+	'.----': '1',
+	'..---': '2',
+	'...--': '3',
+	'....-': '4',
+	'.....': '5',
+	'-....': '6',
+	'--...': '7',
+	'---..': '8',
+	'----.': '9',
+	'-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+
+
+	let firsfArr = [];
+	let secondArr = [];
+	let thirdArr = [];
+	let final = [];
+
+
+
+	for (let i = 0; i <= expr.length; i += 10) {
+		firsfArr.push(expr.slice(i, i + 10));
+	}
+
+	for (let j = 0; j < firsfArr.length; j++) {
+		for (let m = 0; m < firsfArr[j].length; m++) {
+
+			if (firsfArr[j][m] == '1') {
+				secondArr.push(firsfArr[j].slice(firsfArr[j].indexOf(firsfArr[j][m])));
+				break;
+			}
+
+		}
+	}
+
+
+	for (let n = 0; n < secondArr.length; n++) {
+		let param = '';
+		for (let k = 0; k < secondArr[n].length; k += 2) {
+
+			if (secondArr[n].slice(k, k + 2) == '10') {
+				param += '.';
+			} else if (secondArr[n].slice(k, k + 2) == '11') {
+				param += '-';
+			}
+
+		}
+		thirdArr.push(param);
+
+	}
+
+
+	for (let i = 0; i < thirdArr.length; i++) {
+		for (let element in MORSE_TABLE) {
+			if (thirdArr[i] == element) {
+				final.push(MORSE_TABLE[thirdArr[i]])
+			}
+		}
+
+	}
+
+
+
+
+	return final.join('');
+
 }
 
 module.exports = {
-    decode
+	decode
 }
